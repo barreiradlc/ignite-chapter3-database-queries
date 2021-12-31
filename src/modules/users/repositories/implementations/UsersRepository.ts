717525dfb1cb644ff4158b07ef4048b2e20ce12b
Augusto Barreira <barreira266@hotmail.com>
2021-12-30 21:34:58 -0300

    Fetch users ordered by first name

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -23,13 +23,17 @@ export class UsersRepository implements IUsersRepository {
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`
+      SELECT *
+      FROM users
+      ORDER BY first_name ASC;
+    `);
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(``); // Complete usando raw query
   }
 }
